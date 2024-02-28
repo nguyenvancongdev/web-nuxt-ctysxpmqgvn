e1a60fd226f1569c34887f40e736278d87e5a154
congnv <congnv@etc.vn>
2024-02-28 09:16:18 +0700

    update

--- components/user/layout/HeaderNavbar.vue
+++ components/user/layout/HeaderNavbar.vue
@@ -2,13 +2,12 @@
   <div>
     <nav class="fixed top-0 left-0 z-20 w-full border-b border-gray-200 bg-white py-2.5 px-6 sm:px-4">
   <div class="container mx-auto flex max-w-6xl flex-wrap items-center justify-between">
-    <a href="#" class="flex items-center">
-      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="mr-3 h-6 text-blue-500 sm:h-9">
-        <path stroke-linecap="round" stroke-linejoin="round" d="M21 7.5l-9-5.25L3 7.5m18 0l-9 5.25m9-5.25v9l-9 5.25M3 7.5l9 5.25M3 7.5v9l9 5.25m0-9v9" />
-      </svg>
-
-      <span class="self-center whitespace-nowrap text-xl font-semibold">national blanket</span>
-    </a>
+    <NuxtLink to="/" class="flex items-center">
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="mr-3 h-6 text-blue-500 sm:h-9">
+          <path stroke-linecap="round" stroke-linejoin="round" d="M21 7.5l-9-5.25L3 7.5m18 0l-9 5.25m9-5.25v9l-9 5.25M3 7.5l9 5.25M3 7.5v9l9 5.25m0-9v9" />
+        </svg>
+        <span class="self-center whitespace-nowrap text-xl font-semibold">P M Q G</span>
+    </NuxtLink>
     <div class="mt-2 sm:mt-0 sm:flex md:order-2">
       <!-- Login Button -->
       <button type="button" class="rounde mr-3 hidden border border-blue-700 py-1.5 px-6 text-center text-sm font-medium text-blue-700 focus:outline-none focus:ring-4 focus:ring-blue-300 md:inline-block rounded-lg">
