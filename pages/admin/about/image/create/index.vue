1bb52c9feecaeccce719a8bc1d73e71f781c4d06
congnv <congnv@etc.vn>
2024-03-15 16:19:35 +0700

    update

---
+++ pages/admin/about/image/create/index.vue
@@ -0,0 +1,64 @@
+<template>
+    <div>danh sach anh
+
+      <v-file-input
+      accept="image/png, image/jpeg, image/bmp"
+      label="tai anh len"
+      v-model="chosenFile"
+      show-size
+      >
+    </v-file-input>
+    
+    <v-btn @click="update">update</v-btn>
+    <v-btn @click="consoUrl">geturl</v-btn>
+    
+    </div>
+</template>
+<script>
+import { ref, uploadBytes,getDownloadURL,deleteObject } from "firebase/storage";
+import { storage } from "@/fb"
+import uploadFile from '@/mixins/uploadFile'
+export default {
+  name: 'IndexPage',
+  layout: 'admin',
+  mixins: [uploadFile],
+  data() {
+      return {
+        test: '',
+        chosenFile: null,
+      };
+  },
+  // components: {
+  //   VFileInput
+  // },
+  methods: {
+    hanh(){
+        console.log('day la mixin')
+    },
+    
+    async update(){
+    if (this.chosenFile){
+      const imageref = ref(storage,`eme/${this.chosenFile.name}`);
+      uploadBytes(imageref, this.chosenFile).then((snapshot)=>{
+        console.log('snaop', snapshot)
+      })     
+ 
+    }
+    },
+    consoUrl(){
+      const imageref = ref(storage,`eme/favicon.png`);
+      getDownloadURL(imageref).then(res => {
+        console.log('res', res)
+      })
+    },
+    delete(){
+      const desertRef = ref(storage, 'images/desert.jpg');
+      deleteObject(desertRef).then(() => {
+  
+        }).catch((error) => {
+ 
+        });
+      }
+    },
+}
+</script>
